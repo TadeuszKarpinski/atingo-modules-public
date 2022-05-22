7577ca27e3562c734f396dcbf063b61ff74c231e
Tadeusz Karpinski <tadeuszkarpinski@gmail.com>
2022-05-22 15:18:33 +0200

    9.0 mv test modules to public

---
+++ odoo_test/tests/odoo_test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*--
+# © 2022 Atingo Tadeusz Karpiński
+# License OPL-1 (https://www.odoo.com/documentation/15.0/legal/licenses.html).
+
+import requests
+
+from openerp.tests.common import HttpCase, HOST, PORT
+
+class OdooTestHttpCase(HttpCase):
+
+    def setUp(self):
+        super(OdooTestHttpCase, self).setUp()
+        self.opener = requests.Session()
+        self.opener.cookies['session_id'] = self.session_id
+
+    def url_open(self, url, data=None, timeout=10):
+        if url.startswith('/'):
+            url = "http://%s:%s%s" % (HOST, PORT, url)
+        if data:
+            return self.opener.post(url, data=data, timeout=timeout)
+        return self.opener.get(url, timeout=timeout)
