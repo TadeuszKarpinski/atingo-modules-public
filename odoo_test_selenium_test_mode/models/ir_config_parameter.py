56036901b557cfabd76d76aa25753784024496c5
Tadeusz Karpinski <tadeuszkarpinski@gmail.com>
2022-05-22 15:24:47 +0200

    13.0 mv test modules to public

---
+++ odoo_test_selenium_test_mode/models/ir_config_parameter.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*--
+# © 2022 Atingo Tadeusz Karpiński
+# License OPL-1 (https://www.odoo.com/documentation/15.0/legal/licenses.html).
+
+import logging
+
+from odoo import models, api, SUPERUSER_ID
+from odoo.addons.odoo_test_selenium.tests.odoo_test_selenium import (
+    create_browser,
+    SeleniumHttpCase,
+)
+
+_logger = logging.getLogger(__name__)
+
+
+class IrConfigParameter(models.Model):
+    _inherit = "ir.config_parameter"
+
+    @api.model
+    def get_odoo_test_selenium_test_mode(self):
+        return self.with_user(SUPERUSER_ID).get_param(
+            "odoo_test_selenium_test_mode.test_mode"
+        )
---
+++ odoo_test_selenium_test_mode/static/src/js/menu.js
@@ -0,0 +1,69 @@
+// © 2022 Atingo Tadeusz Karpiński
+// License OPL-1 (https://www.odoo.com/documentation/13.0/legal/licenses.html).
+
+odoo.define('odoo_test_selenium_test_mode.TestModeMenu', function (require) {
+    "use strict";
+
+    var DebugManager = require('web.DebugManager');
+    var ajax = require('web.ajax');
+
+    var TestModeMenu = DebugManager.include({
+        start: function () {
+            this._super.apply(this, arguments);
+
+            var self = this;
+            this._rpc({
+                model: 'ir.config_parameter',
+                method: 'get_odoo_test_selenium_test_mode',
+                args: [],
+            }).then(function (param_value) {
+                self.odoo_test_selenium_test_mode = (param_value.toLowerCase() === 'true');
+            });
+            return this;
+        },
+        enter_test_mode: function () {
+            var self = this;
+            self._rpc({
+                model: 'ir.config_parameter',
+                method: 'get_odoo_test_selenium_test_mode',
+                args: [],
+            }).then(function (param_value) {
+                if (param_value.toLowerCase() !== 'false') {
+                    alert("You are already in TEST MODE !");
+                    location.reload();
+                } else {
+                    var session = self.getSession();
+                    session.rpc("/odoo_test_mode_notification", { "channel": "odoo_test_selenium_test_mode", "message": "enter_test_mode" }).then(function () {
+                        setTimeout(function () {
+                            ajax.post('/enter_test_mode', {});
+                            location.reload();
+                        }, 3000);
+                    });
+                }
+            });
+        },
+        leave_test_mode: function () {
+            var self = this;
+            this._rpc({
+                model: 'ir.config_parameter',
+                method: 'get_odoo_test_selenium_test_mode',
+                args: [],
+            }).then(function (param_value) {
+                if (param_value.toLowerCase() !== 'true') {
+                    alert("You aren't in TEST MODE !");
+                    location.reload();
+                } else {
+                    var session = self.getSession();
+                    ajax.post('/leave_test_mode', {});
+
+                    setTimeout(function () {
+                        session.rpc("/odoo_test_mode_notification", { "channel": "odoo_test_selenium_test_mode", "message": "leave_test_mode" }).then(function () {
+                            location.reload();
+                        });
+                    }, 3000);
+                }
+            });
+        },
+    });
+    return TestModeMenu;
+});
