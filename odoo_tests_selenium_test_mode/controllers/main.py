7bffee5f46bc098f386b9bcd661878ada82613ce
Tadeusz Karpiński <tadeuszkarpinski@gmail.com>
2022-05-24 18:36:21 +0000

    12.0 mv odoo_test -> odoo_tests

---
+++ odoo_tests_selenium_test_mode/controllers/main.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*--
+# © 2022 Atingo Tadeusz Karpiński
+# License OPL-1 (https://www.odoo.com/documentation/15.0/legal/licenses.html).
+
+import unittest
+import unittest2
+import logging
+
+from odoo.modules.module import TestStream
+from odoo import http
+from odoo.http import request
+from odoo.addons.odoo_tests_selenium_test_mode.tests.odoo_tests_selenium_test_mode import (
+    OdooTestsSeleniumTestModeTransactionCase,
+)
+
+_logger = logging.getLogger(__name__)
+
+
+class TestSeleniumTestMode(http.Controller):
+    @http.route("/enter_test_mode", type="http", auth="user")
+    def enter_test_mode(self):
+        param = (
+            request.env["ir.config_parameter"]
+            .sudo()
+            .get_param("odoo_tests_selenium_test_mode.test_mode")
+        )
+        if param == "False":
+            request.env["ir.config_parameter"].sudo().set_param(
+                "odoo_tests_selenium_test_mode.test_mode", "True"
+            )
+            t = unittest.TestLoader().loadTestsFromTestCase(
+                OdooTestsSeleniumTestModeTransactionCase
+            )
+            suite = unittest.TestSuite([t])
+            unittest2.TextTestRunner(verbosity=2, stream=TestStream("odoo_tests_selenium_test_mode")).run(suite)
+            self.odoo_tests_mode_notification(
+                "odoo_tests_selenium_test_mode", "leave_test_mode"
+            )
+        return "ok"
+
+    @http.route("/leave_test_mode", type="http", auth="user")
+    def leave_test_mode(self):
+        param = (
+            request.env["ir.config_parameter"]
+            .sudo()
+            .get_param("odoo_tests_selenium_test_mode.test_mode")
+        )
+        if param == "True":
+            request.env["ir.config_parameter"].sudo().set_param(
+                "odoo_tests_selenium_test_mode.test_mode", "False"
+            )
+        return "ok"
+
+    @http.route("/odoo_tests_mode_notification", type="json", auth="user")
+    def odoo_tests_mode_notification(self, channel, message):
+        return request.env["bus.bus"].sendmany([(channel, message)])
