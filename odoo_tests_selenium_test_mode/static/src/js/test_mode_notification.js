09b6952990fb07dc3171a4f9925de861b0bc9ac4
Tadeusz Karpiński <tadeuszkarpinski@gmail.com>
2022-05-24 19:07:21 +0200

    8.0 mv odoo_test -> odoo_tests

---
+++ odoo_tests_selenium_test_mode/static/src/js/test_mode_notification.js
@@ -0,0 +1,40 @@
+// // © 2022 Atingo Tadeusz Karpiński
+// // License OPL-1 (https://www.odoo.com/documentation/12.0/legal/licenses.html).
+
+// odoo.define("odoo_tests_selenium_test_mode.notification", function (require) {
+//     "use strict";
+
+//     var Model = require('web.Model');
+//     var core = require("web.core");
+
+//     var odoo_tests_selenium_test_mode_notification_bar = $('<div class="odoo-test-selenium-test-mode-notification">⚠️⚠️⚠️ You are in test mode ! All changes will be ROLLBACKED ! ⚠️⚠️⚠️</div>');
+//     odoo_tests_selenium_test_mode_notification_bar.hide();
+
+//     function show_odoo_tests_selenium_test_mode_notification_bar() {
+//         var model = new Model('ir.config_parameter');
+//         model.call('get_odoo_tests_selenium_test_mode').then(function (param_value) {
+//             if (param_value.toLowerCase() === 'true') {
+//                 odoo_tests_selenium_test_mode_notification_bar.show();
+//             } else {
+//                 odoo_tests_selenium_test_mode_notification_bar.hide();
+//             }
+//         });
+//     }
+
+//     core.bus.on("web_client_ready", null, function () {
+//         $("body").prepend(odoo_tests_selenium_test_mode_notification_bar);
+//         show_odoo_tests_selenium_test_mode_notification_bar();
+//     });
+
+//     core.bus.on("odoo_tests_selenium_enter_test_mode", null, function () {
+//         setTimeout(function () {
+//             show_odoo_tests_selenium_test_mode_notification_bar();
+//         }, 6000);
+//     });
+
+//     core.bus.on("odoo_tests_selenium_leave_test_mode", null, function () {
+//         setTimeout(function () {
+//             show_odoo_tests_selenium_test_mode_notification_bar();
+//         }, 6000);
+//     });
+// });
---
+++ odoo_tests_selenium_test_mode/static/src/js/web_bus.js
@@ -0,0 +1,52 @@
+// // © 2022 Atingo Tadeusz Karpiński
+// // License OPL-1 (https://www.odoo.com/documentation/12.0/legal/licenses.html).
+
+// odoo.define("OdooTestsSeleniumTestModeWebBus", function (require) {
+//     "use strict";
+
+//     var WebClient = require("web.WebClient");
+//     var core = require("web.core");
+
+//     var bus = require("bus.bus").bus;
+
+//     WebClient.include({
+//         show_application: function () {
+//             var res = this._super();
+//             this.odoo_tests_selenium_test_mode_start_polling();
+//             return res;
+//         },
+//         odoo_tests_selenium_test_mode_start_polling: function () {
+//             this.odoo_tests_selenium_test_mode = "odoo_tests_selenium_test_mode";
+//             this.odoo_tests_selenium_test_mode_all_channels = [
+//                 this.odoo_tests_selenium_test_mode,
+//             ];
+//             bus.add_channel(this.odoo_tests_selenium_test_mode);
+//             bus.on('notification', this, this.odoo_tests_selenium_test_mode_notification);
+//             bus.start_polling();
+//         },
+//         odoo_tests_selenium_test_mode_notification: function (notifications) {
+//             var self = this;
+//             _.each(notifications, function (notification) {
+//                 var channel = notification[0];
+//                 var message = notification[1];
+//                 if (
+//                     self.odoo_tests_selenium_test_mode_all_channels !== null &&
+//                     self.odoo_tests_selenium_test_mode_all_channels.indexOf(channel) > -1
+//                 ) {
+//                     self.odoo_tests_selenium_test_mode_web_bus_on_message(channel, message);
+//                 }
+//             });
+//         },
+//         odoo_tests_selenium_test_mode_web_bus_on_message: function (channel, message) {
+//             if (channel == this.odoo_tests_selenium_test_mode) {
+//                 if (message == "leave_test_mode") {
+//                     core.bus.trigger('odoo_tests_selenium_leave_test_mode');
+//                 }
+//                 if (message == "enter_test_mode") {
+//                     core.bus.trigger('odoo_tests_selenium_enter_test_mode');
+//                 }
+//             }
+//             return true;
+//         },
+//     });
+// });
