15af96581b96d799657131808696ba3c7295d847
Tadeusz Karpiński <tadeuszkarpinski@gmail.com>
2022-05-24 18:45:12 +0000

    13.0 mv odoo_test -> odoo_tests

---
+++ odoo_tests_selenium_test_mode/tests/odoo_tests_selenium_test_mode.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*--
+# © 2022 Atingo Tadeusz Karpiński
+# License OPL-1 (https://www.odoo.com/documentation/15.0/legal/licenses.html).
+
+import logging
+import time
+
+from odoo.tests.common import HttpCase
+
+_logger = logging.getLogger(__name__)
+
+
+class OdooTestsSeleniumTestModeTransactionCase(HttpCase):
+    def test_enter_test_mode(self):
+        _logger.warning(f"Database {self.cr.dbname} is entering Test Mode !")
+        counter = 0
+
+        for i in range(1, 1200):
+            counter = counter + 1
+            if counter >= 10:
+                _logger.warning(f"Database {self.cr.dbname} is in Test Mode !")
+                counter = 0
+            param = (
+                self.env["ir.config_parameter"]
+                .sudo()
+                .get_param("odoo_tests_selenium_test_mode.test_mode")
+            )
+            if param == "False":
+                _logger.warning(f"Database {self.cr.dbname} is leaving Test Mode !")
+                return
+            time.sleep(1)
+        _logger.warning(f"Database {self.cr.dbname} is leaving Test Mode !")
